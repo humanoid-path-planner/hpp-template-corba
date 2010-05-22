5f656663db337bd78e2b163359e4f59e35630926
florent <florent@laas.fr>
2010-05-22 21:08:36 +0200

    Fix typo
    	* include/hpp/corba/template/doc.hh.

--- include/hpp/corba/template/doc.hh
+++ include/hpp/corba/template/doc.hh
@@ -5,7 +5,7 @@
 This package is intended to ease construction of CORBA servers by templating
 actions that are common to all servers.
 
-\section hpp_template_corba_sec_principle
+\section hpp_template_corba_sec_principle Principle
 
 This package only contains one template class hpp::corba::Server. The parameter
 of this template class is the implementation of an idl interface.
